a3806acf8e6be3c80233bf1961de20567571cf7f
Nicolas JATOB <nicolasjatob@orange.fr>
2020-05-19 23:33:19 +0900

    Displaying data received from server

--- app/src/main/java/com/example/project_nicolas_jatob/MainActivity.java
+++ app/src/main/java/com/example/project_nicolas_jatob/MainActivity.java
@@ -33,23 +33,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showList();
         makeApiCall();
     }
 
-    private void showList() {
+    private void showList(List<Granblue_Character> granblueList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }
+
+
+
         // define an adapter
-        mAdapter = new ListAdapter(input);
+        mAdapter = new ListAdapter(granblueList);
         recyclerView.setAdapter(mAdapter);
     }
 
@@ -72,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<RestGranblueResponse> call, Response<RestGranblueResponse> response) {
                 if(response.isSuccessful() && response.body() != null){
                     List<Granblue_Character> characterList = response.body().getResults();
-                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                    showList(characterList);
                 }else
                 {
                     showError();
